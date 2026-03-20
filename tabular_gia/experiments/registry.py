93f795f0a302f92d7e33d6a9f72447d9c203cfe5
ivonilsson <ivo_gote@hotmail.com>
2026-03-20 00:50:56 +0000

    torch modules experiment

--- tabular_gia/experiments/registry.py
+++ tabular_gia/experiments/registry.py
@@ -1,9 +1,11 @@
 from experiments.sweep_runner import SweepExperimentRunner
 from experiments.experiment_fedsgd_batch_sizes import FedSGDBatchSizesRunner
+from experiments.experiment_fedsgd_torch_modules import FedSGDTorchModules
 
 
 EXPERIMENT_RUNNERS = {
     "sweep": SweepExperimentRunner,
-    "fedsgdbatchsizes": FedSGDBatchSizesRunner
+    "fedsgdbatchsizes": FedSGDBatchSizesRunner,
+    "fedsgdtorchmodules": FedSGDTorchModules
 }
 
