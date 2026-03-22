2d9f69d9252fc764aa69bc9c18faae38b3811f13
Maxengw <112926577+Maxengw@users.noreply.github.com>
2026-03-22 10:49:33 +0100

    Add label knowledge experiment

--- tabular_gia/experiments/registry.py
+++ tabular_gia/experiments/registry.py
@@ -1,11 +1,13 @@
 from experiments.sweep_runner import SweepExperimentRunner
 from experiments.experiment_fedsgd_batch_sizes import FedSGDBatchSizesRunner
 from experiments.experiment_attack_iterations import AttackIterationsRunner
+from experiments.experiment_label_knowledge import LabelKnowledgeRunner
 
 
 EXPERIMENT_RUNNERS = {
     "sweep": SweepExperimentRunner,
     "fedsgdbatchsizes": FedSGDBatchSizesRunner,
     "attackiterations": AttackIterationsRunner,
+    "labelknowledge": LabelKnowledgeRunner,
 }
 
